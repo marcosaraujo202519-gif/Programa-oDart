f231e1239a8506b27bc72118b353fa5f8e1d551e
João Marcos <marcosaraujo202519@gmail.com>
2026-08-30 14:27:18 -0300

    04- tipos-explicitos.dart to 04-tipos-explicitos.dart

--- 04- tipos-explicitos.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  String nome = "João"
-    int idade = 24;
-    double altura = 1.70;
-    bool aprovado = true;
-  
-  print('$none | $idade | $altura | $aprovado');
-
-  }
---
+++ 04-tipos-explicitos.dart
@@ -0,0 +1,8 @@
+void main() {
+  String nome = 'Bruno';
+  int idade = 36;
+  double altura = 1.70;
+  bool aprovado = true;
+
+  print('$nome | $idade | $altura | $aprovado');
+}
