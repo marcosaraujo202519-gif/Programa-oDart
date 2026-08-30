169e1db9bd7f34099cdadf4abc87601afc148d61
João Marcos <marcosaraujo202519@gmail.com>
2026-08-30 14:38:23 -0300

    12-switch.dart

---
+++ 12-switch.dart
@@ -0,0 +1,20 @@
+void main() {
+  int opcao = 1;
+
+  switch (opcao) {
+    case 1:
+      print('Cadastrar');
+      break;
+
+    case 2:
+      print('Listar');
+      break;
+
+    case 3:
+      print('Sair');
+      break;
+
+    default:
+      print('Opção inválida');
+  }
+}
