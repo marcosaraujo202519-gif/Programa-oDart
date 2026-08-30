834cdad2506f1940d4c6985871d7353bb705d36a
João Marcos <marcosaraujo202519@gmail.com>
2026-08-30 14:31:24 -0300

    07-ameticos.dardo to 07-aritmeticos.dart

---
+++ 07-aritmeticos.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 10;
+  int b = 3;
+
+  print(a + b); // Adição
+  print(a - b); // Subtração
+  print(a * b); // Multiplicação
+  print(a / b); // Divisão
+  print(a ~/ b); // Divisão inteira
+  print(a % b); // Resto da divisão
+}
